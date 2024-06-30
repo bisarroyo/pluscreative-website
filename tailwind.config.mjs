71dc05da8ed3fd3900a2b77bbb2d1537c8a345d4
Bismark Arroyo <bis.arroyo@gmail.com>
2024-06-29 21:46:43 -0600

    add custom color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,11 @@ export default {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-primary': '#240750'
+      }
+    },
     container: {
       center: true
     }
